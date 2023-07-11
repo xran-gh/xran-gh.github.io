29ca9e920e5ba6d9d4deec5d0ef1b04afd2b30cd
Ran X <xran@mail.com>
2023-07-11 01:08:47 -0400

    css

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import video from '../../assets/contactbackground.mp4'
 
 const Contact = () =>
 {
   return (
-    <div>
-      Contact
+    <div className='Contact'>
+      <div className="content">
+        <video src={video} autoPlay loop muted playsInline/>
+        <div className='center'>
+          contact
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/pages/Resume.js
+++ frontend/src/components/pages/Resume.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import video from '../../assets/resumebackground.mp4'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import {Link} from 'react-scroll';
-import Button from "@mui/material/Button";
+import picture from  '../../assets/resume.png'
 
 const Resume = () =>
 {
@@ -11,20 +9,8 @@ const Resume = () =>
       <div className="content">
         <video src={video} autoPlay loop muted playsInline/>
         <div className='center'>
-          <div className="title">Hello 你好, I'm</div>
-          <div className="name">Ran Xu</div>
-          <div className="subtitle">Software Engineer</div>
+          <img src={picture} alt='resume'/>
         </div>
-        <div className='expand'>
-          <Link to="AboutBody" smooth={true} duration={1000}>
-            <Button disableRipple style={{backgroundColor:'transparent'}}>
-              <ExpandMoreIcon className='icon' />
-            </Button>
-          </Link>
-        </div>
-      </div>
-      <div className='AboutBody'>
-        About My Journey So Far
       </div>
     </div>
   )

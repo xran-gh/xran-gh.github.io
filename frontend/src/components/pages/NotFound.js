e35c6409afc315ade00c2662269155451157c19f
Ran X <xran@mail.com>
2023-07-13 19:24:42 -0400

    error page finished

--- frontend/src/components/pages/NotFound.js
+++ frontend/src/components/pages/NotFound.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import video from '../../assets/errorbackground.mp4'
+import { useInView } from 'react-intersection-observer';
 
 const NotFound = () =>
 {
+  const { ref: codeRef, inView: codeInView } = useInView({threshold: 0.1});
+  const { ref: subtitleRef, inView: subtitleInView } = useInView({threshold: 0.1});
+
   return (
     <div className='NotFound'>
       <div className="content">
         <video src={video} autoPlay loop muted playsInline/>
         <div className='center'>
-          <div className="code">404</div>
+          <div ref={codeRef} className={`${'code'} ${codeInView ? 'animateTitle' : 'OutOfView'}`}>404</div>
           <div className="title">Error</div>
-          <div className="subtitle">Page Not Found</div>
+          <div ref={subtitleRef} className={`${'subtitle'} ${subtitleInView ? 'animateSubtitle' : 'OutOfView'}`}>Page Not Found</div>
         </div>
       </div>
     </div>

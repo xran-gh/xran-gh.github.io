9b92a6d4e870b65cf473c80733cfabb0c73eebdd
Ran X <xran@mail.com>
2023-07-13 02:01:45 -0400

    resume page finished

--- frontend/src/components/pages/Resume.js
+++ frontend/src/components/pages/Resume.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import video from '../../assets/resumebackground.mp4'
 import picture from  '../../assets/resume.png'
+import { useInView } from 'react-intersection-observer';
 
 const Resume = () =>
 {
+  const { ref: resumeRef, inView: resumeInView } = useInView({threshold: 0.1});
+
   return (
     <div className='Resume'>
       <div className="content">
         <video src={video} autoPlay loop muted playsInline/>
-        <div className='center'>
+        <div ref={resumeRef} className={`${'center'} ${resumeInView ? 'animateResume' : 'OutOfView'}`}>
           <img src={picture} alt='resume'/>
         </div>
       </div>

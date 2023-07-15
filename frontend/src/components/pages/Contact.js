0dcbca8bc91ed7132e980c59ea42d96fdee38287
Ran X <xran@mail.com>
2023-07-14 21:54:55 -0400

    contact icons

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import video from '../../assets/contactbackground.mp4'
+import MailIcon from '@mui/icons-material/Email'
+import PhoneIcon from '@mui/icons-material/Phone'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import InstagramIcon from '@mui/icons-material/Instagram'
 
 const Contact = () =>
 {
@@ -8,7 +13,11 @@ const Contact = () =>
       <div className="content">
         <video src={video} autoPlay loop muted playsInline/>
         <div className='center'>
-          contact
+          <MailIcon className='icon'/>
+          <PhoneIcon/>
+          <LinkedInIcon/>
+          <GitHubIcon/>
+          <InstagramIcon/>
         </div>
       </div>
     </div>

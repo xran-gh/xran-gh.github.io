3f35c5042bd87dcd28d5d445eb3e4b479fb95bf4
Ran X <xran@mail.com>
2023-07-16 00:44:16 -0400

    contact page finished

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import video from '../../assets/contactbackground.mp4'
 import MailIcon from '@mui/icons-material/Email'
 import PhoneIcon from '@mui/icons-material/Phone'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import InstagramIcon from '@mui/icons-material/Instagram'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
 
 const Contact = () =>
 {
+  const [openMail, setOpenMail] = useState(false);
+  const [openPhone, setOpenPhone] = useState(false);
+
   return (
     <div className='Contact'>
       <div className="content">
         <video src={video} autoPlay loop muted playsInline/>
         <div className='center'>
-          <MailIcon className='icon'/>
-          <PhoneIcon/>
-          <LinkedInIcon/>
-          <GitHubIcon/>
-          <InstagramIcon/>
+          <Button onClick={() => setOpenMail(true)}>
+            <MailIcon className='icon'/>
+          </Button>
+          <Dialog open={openMail} onClose={() => setOpenMail(false)}>
+            <DialogTitle>Email Address</DialogTitle>
+            <DialogContent>
+              <DialogContentText>xran@mail.com</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => setOpenMail(false)}>Close</Button>
+            </DialogActions>
+          </Dialog>
+          <Button onClick={() => setOpenPhone(true)}>
+            <PhoneIcon className='icon'/>
+          </Button>
+          <Dialog open={openPhone} onClose={() => setOpenPhone(false)}>
+            <DialogTitle>Phone Number</DialogTitle>
+            <DialogContent>
+              <DialogContentText>(561) 755-1172</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => setOpenPhone(false)}>Close</Button>
+            </DialogActions>
+          </Dialog>
+          <Button href='https://www.linkedin.com/in/ranxu-cs/' target='_blank' rel="noreferrer">
+            <LinkedInIcon className='icon'/>
+          </Button>
+          <Button href='https://github.com/xran-gh' target='_blank' rel="noreferrer">
+            <GitHubIcon className='icon'/>
+          </Button>
+          <Button href='https://www.instagram.com/xran.ig/' target='_blank' rel="noreferrer">
+            <InstagramIcon className='icon'/>
+          </Button>
         </div>
       </div>
     </div>

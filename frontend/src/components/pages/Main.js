e2fb6596731ee4ac8b268b9bd9e5e985cfeff0f7
Ran X <xran@mail.com>
2023-07-31 12:04:25 -0400

    reformat

--- frontend/src/components/pages/Main.js
+++ frontend/src/components/pages/Main.js
@@ -4,21 +4,17 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import { Link } from 'react-scroll';
 import Button from "@mui/material/Button";
 import PersonIcon from '@mui/icons-material/Person'
-import SchoolIcon from '@mui/icons-material/School'
-import LightbulbIcon from '@mui/icons-material/Lightbulb'
 import { useInView } from 'react-intersection-observer';
-import { Link as DomLink} from 'react-router-dom';
 
 const Main = () =>
 {
   const { ref: titleRef, inView: titleInView } = useInView({threshold: 0.9});
   const { ref: subtitleRef, inView: subtitleInView } = useInView({threshold: 0.9});
   const { ref: aboutRef, inView: aboutInView } = useInView({threshold: 0.2});
-  const { ref: courseRef, inView: courseInView } = useInView({threshold: 0.2});
-  const { ref: projectRef, inView: projectInView } = useInView({threshold: 0.2});
 
   return (
     <div className='Main'>
+
       <div className="content">
         <video src={video} autoPlay loop muted playsInline/>
         <div className='center'>
@@ -34,6 +30,7 @@ const Main = () =>
           </Link>
         </div>
       </div>
+
       <div ref={aboutRef} className={`${'AboutCard'} ${aboutInView ? 'animateAbout' : 'OutOfView'}`}>
         <div className='top'>
           <PersonIcon fontSize='inherit' sx={{paddingRight: "0.2vw"}}/>
@@ -49,33 +46,9 @@ const Main = () =>
               pictures
             </div>
           </div>
-          <DomLink to="/about">
-            <button className='mainBtn' onClick={()=>{window.scroll(0, 0)}}>Learn More</button>
-          </DomLink>
         </div>
       </div>
-      <div ref={courseRef} className={`${'CoursesCard'} ${courseInView ? 'animateCourse' : 'OutOfView'}`}>
-        <div className='top'>
-          <SchoolIcon fontSize='inherit' sx={{paddingRight: "0.2vw"}}/>
-          Programming Courses
-        </div>
-        <div className='center'>
-          <DomLink to="/course">
-            <button className='mainBtn' onClick={()=>{window.scroll(0, 0)}}>Learn More</button>
-          </DomLink>
-        </div>
-      </div>
-      <div ref={projectRef} className={`${'ProjectCard'} ${projectInView ? 'animateProject' : 'OutOfView'}`}>
-        <div className='top'>
-          <LightbulbIcon fontSize='inherit' sx={{paddingRight: "0.2vw"}}/>
-          Projects And Ideas
-        </div>
-        <div className='center'>
-          <DomLink to="/project">
-            <button className='mainBtn' onClick={()=>{window.scroll(0, 0)}}>Learn More</button>
-          </DomLink>
-        </div>
-      </div>
+
     </div>
   )
 }
